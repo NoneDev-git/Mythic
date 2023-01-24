c5869dc185b3d83c1d35168a6e7bde076fbf3fb8
なし <77238864+NoneDev-git@users.noreply.github.com>
2023-01-24 23:50:54 +0500

    Add files via upload

---
+++ install_all.sh
@@ -0,0 +1,25 @@
+./mythic-cli install github https://github.com/MythicC2Profiles/dns.git
+./mythic-cli install github https://github.com/MythicC2Profiles/websocket.git
+./mythic-cli install github https://github.com/MythicC2Profiles/smb.git
+./mythic-cli install github https://github.com/MythicC2Profiles/http.git
+./mythic-cli install github https://github.com/MythicC2Profiles/dynamichttp.git
+./mythic-cli install github https://github.com/MythicC2Profiles/tcp.git
+./mythic-cli install github https://github.com/MythicAgents/apfell.git
+./mythic-cli install github https://github.com/MythicAgents/Apollo.git
+./mythic-cli install github https://github.com/MythicAgents/Athena.git
+./mythic-cli install github https://github.com/MythicAgents/freyja.git
+./mythic-cli install github https://github.com/MythicAgents/hermes.git
+./mythic-cli install github https://github.com/MythicAgents/leviathan.git
+./mythic-cli install github https://github.com/MythicAgents/Medusa.git
+./mythic-cli install github https://github.com/MythicAgents/merlin.git
+./mythic-cli install github https://github.com/MythicAgents/orthrus.git
+./mythic-cli install github https://github.com/MythicAgents/poseidon.git
+./mythic-cli install github https://github.com/MythicAgents/tetanus.git
+./mythic-cli install github https://github.com/MythicAgents/typhon.git
+./mythic-cli install github https://github.com/MythicAgents/venus.git
+./mythic-cli install github https://github.com/MythicAgents/zippy.git
+./mythic-cli install github https://github.com/MythicAgents/Nimplant.git
+./mythic-cli install github https://github.com/MythicAgents/atlas.git
+./mythic-cli install github https://github.com/MythicAgents/hercules.git
+
+
